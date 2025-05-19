0cf12774d4efc46a0c91ea4aa726b81d1cdbea1c
Ruhama Fikre <ruhamis.projects985@gmail.com>
2025-05-20 01:13:55 +0300

    error logging

--- mood_and_meal_analyser/analyze_mood.php
+++ mood_and_meal_analyser/analyze_mood.php
@@ -1,5 +1,9 @@
 <?php
-
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/php_errors.log');
 session_start();
 header('Content-Type: application/json');
 require_once '../config/db.php';
